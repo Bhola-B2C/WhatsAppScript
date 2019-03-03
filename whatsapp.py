7197e174779ec847a73410f1550c7dfba2ce8c5f
Bhola-B2C <bholajrs@gmail.com>
2019-03-03 19:52:42 +0530

    Add support for firefox

--- whatsapp.py
+++ whatsapp.py
@@ -5,19 +5,22 @@ import time
 import sys
 
 """ for firefox """
-#driver = webdriver.Firefox(executable_path = './firefox/geckodriver')
+# profile = webdriver.FirefoxProfile('/home/sirius0027/.mozilla/firefox/m2iyw7tf.default')
+# driver = webdriver.Firefox(executable_path = '../firefox/geckodriver', firefox_profile = profile)
 
 """ for chrome """
 options = webdriver.ChromeOptions()
 options.add_argument('user-data-dir=/home/sirius0027/.config/google-chrome/')
 driver = webdriver.Chrome(executable_path = '../chrome/chromedriver', options = options)
+
 driver.implicitly_wait(60)
 target = sys.argv[1]
 message = sys.argv[2]
 try:
     driver.get("https://web.whatsapp.com/")
     x_arg = '//span[@title="{}"]'.format(target)
-    title = driver.find_element_by_xpath(x_arg)
+    for i in range(3):
+        title = driver.find_element_by_xpath(x_arg)
     title.click()
     inp_xpath = '//div[@dir="ltr"][@data-tab="1"][@spellcheck="true"]'
     input_box = driver.find_element_by_xpath(inp_xpath)
